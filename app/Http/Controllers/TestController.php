7d62b9524dd839f344d77a10fc4774f2bcf75b25
JokerLinly <JokerLinly@gmail.com>
2016-04-02 16:07:13 +0800

    测试成功

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,11 +10,22 @@ use EasyWeChat;
 
 class TestController extends Controller {
 
-    public function serve(Application $app)
-    {
-        $staff = $app->staff;
-        $staff->lists();
-        $staff->onlines();
-        dd($staff);
+    public function index()
+   {
+	//$userService  = EasyWeChat::user();
+	//$userService->lists($nextOpenId = null); 
+        $notice = EasyWeChat::notice();
+       	$userId = 'od2TLjssFOknDLcsKzw0kSkqF5d8';
+	$templateId = 'yzXYTlKj78bfMZIeHHRwkUx9acyLJjiKUg3h0uzWsE0';
+	$url = 'http://120.27.104.83/';
+	$color = '#FF0000';
+	$data = array(
+       		"comment"  => "这里是骏哥哥测试",
+         	"remark" => "点击查看详情",
+        );
+
+	$messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($userId)->send();
+
+    	//dd( $userService);
     }
 }
