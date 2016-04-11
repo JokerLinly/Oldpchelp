7d71ddfd327711db0cd2cc1e88cd8a33807d6772
JokerLinly <JokerLinly@gmail.com>
2016-04-11 21:44:13 +0800

    管理员订单列表

--- resources/views/Admin/list.blade.php
+++ resources/views/Admin/list.blade.php
@@ -5,35 +5,42 @@
 @extends('body')
 @section('main')
 
+@if($tickets->count())
 <section class="mainContain">
-@if ($tickets->count())
+
     @foreach ($tickets as $ticket)
     
-        <a href="{{ URL('adminticketdata/'.$ticket->id.'/show/') }}" class="block pad1r lh2 borderB pr" style="background: #fff;">
+        <a href="{{ URL('mytickets/'.$ticket->id.'/show/') }}" class="block pad1r lh2 borderB pr" style="background: #fff;">
             <p class="clearfix color2f">
-                <span class="fl font14" style="width: 90%;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">{{ $ticket->problem }}</span>
-               
-            </p>
-            <span class="glyphicon glyphicon-chevron-right" style="float: right;position: absolute;right: 1rem;top: 40%;" aria-hidden="true">负责人：{{$ticket->pcer->name}}</span>         
+                <span class="fl font14" style="width: 70%;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">{{$ticket->problem}}</span>
+                <span class="fr font13 marR3r">
+                  @if     (($ticket->state)==1)<td>{{'处理中'}}</td>
+                  @endif
 
-            <p class="clearfix color60">
-                <span class="fl font12" style="width: 90%;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
                 </span>
-
-                </p>
-
-        
+                
+            </p>
+            <span class="glyphicon glyphicon-chevron-right" style="float: right;position: absolute;right: 1rem;top: 40%;" aria-hidden="true"></span>
+            <p class="clearfix color60">
+                <span class="fl font12" style="width: 55%;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">报修至今：{{ $ticket->differ_time }}</span>
+                <span class="fr font12 marR3r">负责人：
+                @if($ticket->pcer) {{ $ticket->pcer->name}}
+                @else 暂无
+                @endif
+                </span>
+            </p>
         </a> 
     @endforeach
-    @else
-        <div class="container">
-            <div class="content">
-                <div class="title">亲(づ￣3￣)づ╭❤～ 你还没有未完成订单喔！</div>
-            </div>
+
+</section>
+@else
+    <div class="container">
+        <div class="content">
+            <div class="title">亲(づ￣3￣)づ╭❤～ 你还没有分配过订单喔！</div>
         </div>
+    </div>
         
 @endif
-</section>
 
 <div class="row-fluid">
     <div class="span12 navbar-fixed-bottom footer" >
