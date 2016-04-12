883f6b82ad3f67af0c2d5548c845895054d24c7e
JokerLinly <JokerLinly@gmail.com>
2016-04-12 16:30:12 +0800

    测试魅族手机样式问题

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
