ecbeb14b83b0ac6ca48a3754e013e2ad2a35bacd
JokerLinly <JokerLinly@gmail.com>
2016-08-26 15:08:56 +0800

    网页授权获取用户基本信息调试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -35,11 +35,10 @@ class TestController extends Controller {
 
         // 获取 OAuth 授权结果用户信息
         $user = $oauth->user();
-        dd($_SESSION['target_url']);
         $_SESSION['wechat_user'] = $user->toArray();
 
         $targetUrl = empty($_SESSION['target_url']) ? '/test' : $_SESSION['target_url'];
-        return Redirect::back('location:'.$targetUrl);
+        return Redirect::back();
         // header('location:'. $targetUrl); // 跳转到 user/profile
     }
 
