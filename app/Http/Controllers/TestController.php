2db211d195d92688b021d22a1721a0f487fb6fd7
JokerLinly <JokerLinly@gmail.com>
2016-08-26 19:35:38 +0800

    测试跳转

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,7 +22,7 @@ class TestController extends Controller {
         $openid = $user->getId();
         $_SESSION['wechat_user'] = $user->toArray();
                 
-        return Redirect::action('App\Http\Controllers\Ticket\HomeController@index',array('openid'=>$openid));
+        return Redirect::action('Ticket\HomeController@index',array('openid'=>$openid));
     }
 
 }
