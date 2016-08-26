31257ffa5df6bda582090b18f8b376a5cca4bd50
JokerLinly <JokerLinly@gmail.com>
2016-08-26 17:05:10 +0800

    网页授权测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,7 +16,7 @@ class TestController extends Controller {
         $data = $request->session()->all();
         // 未登录
         if (empty($_SESSION['wechat_user']) || !$request->has('wechat_user')) {
-
+            dd($oauth->redirect());
           return $oauth->redirect();
           // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
           // $oauth->redirect()->send();
