96f09ceab057cc2c7a350996189a90f5b33e22cc
JokerLinly <JokerLinly@gmail.com>
2016-08-26 12:45:51 +0800

    网页授权获取用户基本信息调试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,10 +12,27 @@ class TestController extends Controller {
 
     public function index(Application $app)
    {
-      dd($_POST);
-      $user = $app->user; 
+        $oauth = $app->oauth;
 
-      dd($user);
-    	//dd( $userService);
+        // 未登录
+        if (empty($_SESSION['wechat_user'])) {
+
+          $_SESSION['target_url'] = 'user/profile';
+
+          // return $oauth->redirect();
+          // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+          $oauth->redirect()->send();
+        }
+
+        // 已经登录过
+        $user = $_SESSION['wechat_user'];
+
+        	//dd( $userService);
     }
+
+    public function redirectBack(){
+
+    }
+
+
 }
