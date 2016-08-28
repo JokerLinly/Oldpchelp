9892c755dae3c478126231e591c6ae15652be87e
JokerLinly <JokerLinly@gmail.com>
2016-08-28 13:59:40 +0800

    创建订单之后的动作

--- app/Http/Controllers/Ticket/HomeController.php
+++ app/Http/Controllers/Ticket/HomeController.php
@@ -104,7 +104,7 @@ class HomeController extends Controller
                 "remark"  => "点击查看详情",
             );
             $messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($openid)->send();*/
-            return Redirect::action('Ticket\TicketController@show',array('wcuser_id'=>$request->input('wcuser_id')));
+            return Redirect::action('Ticket\HomeController@show',array('wcuser_id'=>$request->input('wcuser_id')));
         } else {
              return Redirect::back()->withInput()->with('message', '报修失败，请重新报修');
         }     
