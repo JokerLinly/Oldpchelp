90dacbda3fa0dfe4da13331fc73d52e204ef8a01
JokerLinly <JokerLinly@gmail.com>
2016-08-28 09:13:02 +0800

    创建订单之后的动作

--- app/Http/Controllers/Ticket/TicketController.php
+++ app/Http/Controllers/Ticket/TicketController.php
@@ -22,7 +22,7 @@ class TicketController extends Controller
      * @return [type]              [description]
      */
     public function index(Request $request){
-        dd($request->wcuser_id);
+        dd($request);
 
         $wcuser_id = $request->wcuser_id;
         $wcuser = WcuserModule::getWcuserByCondition('id','id',$wcuser_id);
