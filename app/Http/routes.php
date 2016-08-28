6cba7d8047416d9670ac4aacfe4fe0713275b7c5
JokerLinly <JokerLinly@gmail.com>
2016-08-28 15:04:43 +0800

    what

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,80 +35,80 @@ Route::resource('/ticket','Ticket\TicketController');
 /*用户的订单*/
 Route::get('/mytickets', 'WechatController@mytickets');
 
-Route::controller('/mytickets','Ticket\TicketController');
+// Route::controller('/mytickets','Ticket\TicketController');
 
-Route::get('/', 'TestController@index');
-
-
-/*PC仔*/
-Route::controller('/pcer/{openid}','Member\HomeController');
+// Route::get('/', 'TestController@index');
 
 
-/*PC仔的订单*/
-Route::get('/pcertickets/{openid}/main','Member\TicketController@main');
-Route::get('/pcertickets/{openid}/listory','Member\TicketController@listory');
-Route::controller('/pcertickets/{openid}/{id}','Member\TicketController');
-
-/*PC管理员微信订单*/
-Route::get('/pcadminwc/{openid}/listory','Admin\TicketController@listory');
-Route::controller('/pcadminwc/{openid}/{id}','Admin\TicketController');
-Route::controller('/pcadminwc/{openid}','Admin\TicketController');
+// /*PC仔*/
+// Route::controller('/pcer/{openid}','Member\HomeController');
 
 
+// /*PC仔的订单*/
+// Route::get('/pcertickets/{openid}/main','Member\TicketController@main');
+// Route::get('/pcertickets/{openid}/listory','Member\TicketController@listory');
+// Route::controller('/pcertickets/{openid}/{id}','Member\TicketController');
 
-/*PC管理员Web后台*/
-$router->group(['namespace'=>'Admin','prefix'=>'pcadmin'], function() {
-    Route::get('','HomeController@index');
-    Route::post('/login','HomeController@login');
+// /*PC管理员微信订单*/
+// Route::get('/pcadminwc/{openid}/listory','Admin\TicketController@listory');
+// Route::controller('/pcadminwc/{openid}/{id}','Admin\TicketController');
+// Route::controller('/pcadminwc/{openid}','Admin\TicketController');
+
+
+
+// /*PC管理员Web后台*/
+// $router->group(['namespace'=>'Admin','prefix'=>'pcadmin'], function() {
+//     Route::get('','HomeController@index');
+//     Route::post('/login','HomeController@login');
     
-});
+// });
 
-$router->group(['namespace'=>'Admin','prefix'=>'pcadmin','middleware'=>'pcadmin_login'], function() {
+// $router->group(['namespace'=>'Admin','prefix'=>'pcadmin','middleware'=>'pcadmin_login'], function() {
    
-    Route::get('ticketlock/{id}','TicketController@ticketlock');
-    Route::post('ticketpcer','TicketController@pcersingle');
-    Route::post('ticketspcer','TicketController@pcerall');
-    /*首页订单end*/
+//     Route::get('ticketlock/{id}','TicketController@ticketlock');
+//     Route::post('ticketpcer','TicketController@pcersingle');
+//     Route::post('ticketspcer','TicketController@pcerall');
+//     /*首页订单end*/
 
-    Route::get('mytickets/unset','TicketController@ticketsunset');
-    Route::post('mytickets/unset/beforeset','TicketController@beforeset');
-    Route::post('mytickets/sentMessage','TicketController@sentMessage');
-    Route::post('mytickets/overticket','TicketController@overticket');
-    Route::controller('mytickets','TicketController');
-     /*首页订单*/
-    Route::controller('','HomeController');
-});
+//     Route::get('mytickets/unset','TicketController@ticketsunset');
+//     Route::post('mytickets/unset/beforeset','TicketController@beforeset');
+//     Route::post('mytickets/sentMessage','TicketController@sentMessage');
+//     Route::post('mytickets/overticket','TicketController@overticket');
+//     Route::controller('mytickets','TicketController');
+//      /*首页订单*/
+//     Route::controller('','HomeController');
+// });
 
-/*骏哥哥后台*/
+// /*骏哥哥后台*/
 
-$router->group(['namespace'=>'Super','prefix'=>'super'], function() {
-    Route::get('','HomeController@index');
-    Route::post('/login','HomeController@login');
+// $router->group(['namespace'=>'Super','prefix'=>'super'], function() {
+//     Route::get('','HomeController@index');
+//     Route::post('/login','HomeController@login');
     
-});
+// });
 
-$router->group(['namespace'=>'Super','prefix'=>'super','middleware'=>'login_session'], function() {
-    /*PC队员 start*/
-    Route::get('pcer','PcerController@index');
-    Route::get('pcerset/{id}','PcerController@set');
-    Route::get('pcadminset/{id}','PcadminController@set');
-    Route::get('pcset','PcerController@levelset');
-    Route::post('pcset/leveladd','PcerController@leveladd');
-    Route::post('pcset/leveldel','PcerController@leveldel');
-    Route::get('pcset/levelshow/{id}','PcerController@show');
-    // Route::get('menu','WechatController@menuShow');
-    /*PC队员 end*/
+// $router->group(['namespace'=>'Super','prefix'=>'super','middleware'=>'login_session'], function() {
+//     /*PC队员 start*/
+//     Route::get('pcer','PcerController@index');
+//     Route::get('pcerset/{id}','PcerController@set');
+//     Route::get('pcadminset/{id}','PcadminController@set');
+//     Route::get('pcset','PcerController@levelset');
+//     Route::post('pcset/leveladd','PcerController@leveladd');
+//     Route::post('pcset/leveldel','PcerController@leveldel');
+//     Route::get('pcset/levelshow/{id}','PcerController@show');
+//     // Route::get('menu','WechatController@menuShow');
+//     /*PC队员 end*/
 
 
-    /*订单管理 start*/
-    Route::get('tickets','TicketController@index');
-    /*订单管理 end*/
+//     /*订单管理 start*/
+//     Route::get('tickets','TicketController@index');
+//     /*订单管理 end*/
 
-    /*首页*/
-    Route::controller('','HomeController');
-    /*首页end*/
+//     /*首页*/
+//     Route::controller('','HomeController');
+//     /*首页end*/
 
-});
+// });
 
 /*
 |--------------------------------------------------------------------------
